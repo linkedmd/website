ec9940ace1929467afd0f5e3f359008118530c17
Luis Cuende <me@luisivan.net>
2022-10-17 15:04:41 +0100

    fix(): Adding mimetype to API response

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -2,5 +2,6 @@ export default async function handler(req, res) {
   const file = await fetch(
     `https://raw.githubusercontent.com/${req.query.path}`
   )
+  res.setHeader('Content-Type', 'text/markdown')
   res.status(200).send(await file.text())
 }
