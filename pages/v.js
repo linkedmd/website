17515eecf14afcc6c57d4996d9edf62f7dedaaf6
Luis Cuende <me@luisivan.net>
2022-11-24 10:30:54 +0000

    feat(): Set page title

--- pages/v.js
+++ pages/v.js
@@ -19,7 +19,13 @@ export default function View() {
       {u && (
         <div>
           <Script src="https://hypothes.is/embed.js" />
-          <LinkedMarkdownViewer fileURI={u} />
+          <LinkedMarkdownViewer
+            fileURI={u}
+            onFileURIChange={() => {
+              const h1 = document.getElementsByTagName('h1')[0]
+              if (h1) document.title = h1.innerText
+            }}
+          />
         </div>
       )}
     </main>
